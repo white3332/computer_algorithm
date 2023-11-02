f39ebd4a4d7c5111007d084ca7f35964526649bb
white3332 <129389029+white3332@users.noreply.github.com>
2023-11-02 12:59:28 +0900

    Delete sorted_2d_array.py

--- sorted_2d_array.py
+++
@@ -1,6 +0,0 @@
-my_2d_array = [[1, 2], [2, 1], [1, 1], [0, 0], [0, 1]]
-
-sorted_array = sorted(my_2d_array, key=lambda x: (x[0], x[1]))  # 두 번째 요소를 기준으로 오름차순 정렬, 그 다음 첫 번째 요소를 기준으로 정렬
-
-for item in sorted_array:
-    print(item)
